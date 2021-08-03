acaac58a1f26d02d54197e5d17ad80bdd34e394c
NancyStone <iamshinan@gmail.com>
2021-08-03 23:23:34 +1000

    Checked whether orders.json exists.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,16 @@
 import express = require('express');
+import * as fs from 'fs';
+
 import apiRouter from './routes';
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(apiRouter);
+
+if (!fs.existsSync('src/server/data/orders.json')) {
+  fs.writeFileSync('src/server/data/orders.json', '');
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
